c43071540126e7779c77a886721c55c01a0bd9c7
Logdevweb <logandejond@gmail.com>
2024-05-23 14:00:42 +0200

    sql inner join author
    vid6

--- app/models/booksModel.php
+++ app/models/booksModel.php
@@ -4,9 +4,10 @@ namespace App\Models\BooksModel;
 use \PDO;
 
 function findAll(PDO $connexion): array {
-    $sql = "select *
-    from books
-    order by created_at desc
+    $sql = "select *, b.id as bookID, a.id as authorID
+    from books b
+    inner join authors a on b.author_id = a.id
+    order by b.created_at desc
     limit 3;";
     return $connexion->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 }
